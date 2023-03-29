54e221e46e2046f3c4e3010ec4337a19e1a2ae30
Rakshit Malik <rakshitmalik156@gmail.com>
2023-03-29 12:58:16 +0530

    commi

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 img = "";
+status = "";
 function preload(){
     img = loadImage("img/dog_cat.jpg");
 }
 function setup(){
     canvas = createCanvas(640,420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd",modelloaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
 }
 function draw(){
     image(img,0,0,640,420);
@@ -13,4 +16,22 @@ function draw(){
     noFill();
     stroke("#ff0000");
     rect(30,60,450,350);
+    fill("#ff0000");
+    text("cat",310,105);
+    noFill();
+    stroke("#ff0000");
+    rect(300,90,270,310);
+}
+function modelloaded(){
+    console.log("cocossd loaded");
+    objectDetector.detect(img,gotResults);
+    status = true;
+}
+function gotResults(error,results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+    }
 }
