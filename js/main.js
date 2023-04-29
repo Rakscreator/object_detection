88f893fca804ca5a4ddbe9e7c0ebcfdadb389e03
Rakshit Malik <rakshitmalik156@gmail.com>
2023-04-29 16:04:30 +0530

    commit

--- js/main.js
+++ js/main.js
@@ -1,33 +1,41 @@
 img = "";
 status = "";
 objects = [];
-function preload(){
-    img = loadImage("img/dog_cat.jpg");
-}
+function preload(){}
 function setup(){
-    canvas = createCanvas(640,420);
+    canvas = createCanvas(640,470);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(640,470);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,640,470);
+    if(status != ""){
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video,gotResults);
+        for (i = 0; i < objects.length; i++) {
+            percent = floor(objects[i].confidence * 100);
+            document.getElementById("status").innerHTML = "Status : Object(s) Detected"
+            document.getElementById("no_of_objects").innerHTML = "No. of object(s) Dectected : " + objects.length;
+            fill(r,g,b);
+            text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x-10,objects[i].y-20,objects[i].width,objects[i].height);
+            
+        }
+    }
+}
+function start(){
     objectDetector = ml5.objectDetector("cocossd",modelloaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects"
 }
-function draw(){
-image(img,0,0,640,420);
-if(status != ""){
-    for (i = 0; i < objects.length; i++) {
-        percent = floor(objects[i].confidence * 100);
-        document.getElementById("status").innerHTML = "Status : Object(s) Detected"
-        fill("#FF0000");
-        text(objects[i].label+" "+percent+"%",objects[i].x,objects[i].y);
-        noFill();
-        stroke("#FF0000");
-        rect(objects[i].x-10,objects[i].y-20,objects[i].width,objects[i].height);
-        
-    }
-}
-}
 function modelloaded(){
     console.log("cocossd loaded");
-    objectDetector.detect(img,gotResults);
     status = true;
 }
 function gotResults(error,results){
